903990aaf731919ededa0f6c6e7134822c097b99
Алексей <96423703+AlexFerji@users.noreply.github.com>
2022-05-18 18:37:43 +0300

    Update settings.py
    update

--- mod/settings.py
+++ mod/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD': '394822sid',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
