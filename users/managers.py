fa56303498ff6998b1c800286b004092ef989526
Алексей <96423703+AlexFerji@users.noreply.github.com>
2022-05-24 16:16:43 +0300

    Update managers.py
    +

--- users/managers.py
+++ users/managers.py
@@ -15,7 +15,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_('Адрес электронной почты должен быть установлен'))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.set_password()
+        user.set_password(password)
         user.save()
         return user
 
